7d2e450ecdd9ac0dbbeae35dd3dcbd158bd5e61e
jcontour <contj186@newschool.edu>
2017-10-25 20:44:27 -0400

    change port in index.js

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
   console.log("url accessed");
   console.log("request", request);
@@ -22,6 +12,6 @@ app.post('/brivo', function(request, response){
 	console.log(request);
 });
 
-app.listen(app.get('port'), function() {
+app.listen(process.env.PORT, function() {
   console.log('Node app is running on port', app.get('port'));
 });
