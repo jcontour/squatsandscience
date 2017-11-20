6bdfa21d6b72d70d4065b6dea770c12a2849375d
jcontour <contj186@newschool.edu>
2017-11-20 15:00:55 -0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.get('/', function(request, response) {
-  console.log("url accessed");
-  console.log("request", request);
-  console.log("response", response);
-  // response.render('pages/index')
-});
-
-app.post('/brivo', function(request, response){
-	console.log(request);
-});
-
-app.listen(process.env.PORT, function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
